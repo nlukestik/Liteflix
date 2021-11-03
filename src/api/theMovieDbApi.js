f1a056fa8e05e0bf093136136ae9b763d0f7d97d
Nicolas Lukestik <nicolaslukestik@gmail.com>
2021-11-03 19:52:01 -0300

    Improve fetch url

--- src/api/theMovieDbApi.js
+++ src/api/theMovieDbApi.js
@@ -1,6 +1,6 @@
 export const getPopularMovies = async() => {
 	try {
-		const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_MOVIE_DB_API_KEY}`)
+		const response = await fetch(`${window.location.protocol}//api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_MOVIE_DB_API_KEY}`)
 		const data = await response.json()
 		return data?.results
 	} 
@@ -11,7 +11,7 @@ export const getPopularMovies = async() => {
 
 export const getFeaturedMovies = async() => {
 	try {
-		const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_MOVIE_DB_API_KEY}`)
+		const response = await fetch(`${window.location.protocol}//api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_MOVIE_DB_API_KEY}`)
 		const data = await response.json()
 		return data?.results
 	} 
